1e253642303fd3b8fe178121a949c97ff644ac3d
chick_0 <chick_0@kakao.com>
2021-01-16 11:07:32 +0900

    신고파일 경고&차단 기능 추가

--- app/views/dl.py
+++ app/views/dl.py
@@ -4,12 +4,14 @@ from json import dumps
 from datetime import datetime, timedelta
 
 from flask import Blueprint
+from flask import request, session
 from flask import render_template
 from flask import abort, Response
+from flask import redirect, url_for
 from sqlalchemy.exc import OperationalError
 
 from app import db
-from models import File
+from models import File, Report
 
 bp = Blueprint(
     name=__name__.split(".")[-1],
@@ -24,8 +26,14 @@ def get_file_by_idx(idx: str):
     ).first()
 
 
+def get_report_by_hash(md5: str):
+    return Report.query.filter_by(
+        md5=md5
+    ).first()
+
+
 @bp.route("/<string:idx>/md5")
-def md5(idx: str):
+def _md5(idx: str):
     try:
         ctx = get_file_by_idx(idx=idx)
     except OperationalError:
@@ -55,10 +63,34 @@ def ask(idx: str):
     if ctx is None:
         abort(404)
 
+    report = get_report_by_hash(md5=ctx.md5)
+    print(report)
+    if report is not None:
+        try:
+            session[ctx.md5]
+        except KeyError:
+            session[ctx.md5] = False  # True: download, False: Cancel
+
+        if session[ctx.md5] is False:
+            return render_template(
+                "dl/warn.html",
+                ctx=ctx,
+                report=report
+            )
+
     return render_template(
         "dl/ask.html",
         ctx=ctx
     )
+
+
+@bp.route("/warn_off/<string:idx>/<string:md5>")
+def warn_off(idx: str, md5: str):
+    if request.referrer is None:
+        abort(400)
+    session[md5] = True
+
+    return redirect(url_for(".ask", idx=idx))
 
 
 @bp.route("/<string:idx>/<string:filename>")
@@ -82,6 +114,14 @@ def download(idx: str, filename: str):
             )
         except (FileNotFoundError, PermissionError, Exception):
             pass
+
+    report = get_report_by_hash(md5=ctx.md5)
+    if report is not None:
+        if report.ban is True:
+            return render_template(
+                "error/error.html",
+                message="이 파일은 차단된 파일입니다"
+            ), 400
 
     if path.exists(path.join("upload", idx)):
         try:
