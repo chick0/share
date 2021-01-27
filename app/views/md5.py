387a0d5320ee9ccb7e7b6c3c6d5402a22faeb82b
chick_0 <chick_0@kakao.com>
2021-01-27 23:42:29 +0900

    csrf 토큰 제거

--- app/views/md5.py
+++ app/views/md5.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from secrets import token_bytes
 
 from flask import Blueprint
-from flask import request, session
+from flask import request
 from flask import redirect, url_for
 from flask import abort, render_template
 from sqlalchemy.exc import IntegrityError
@@ -21,10 +20,8 @@ bp = Blueprint(
 @bp.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == "GET":
-        session['csrf_md5_search'] = token_bytes(64).hex()
         return render_template(
-            "md5/search.html",
-            csrf=session['csrf_md5_search']
+            "md5/search.html"
         )
     if request.method == "POST":
         if request.referrer is None:
@@ -34,13 +31,12 @@ def index():
             if len(request.form['md5']) < 2:
                 abort(400)
 
-            if request.form['csrf_token'] == session['csrf_md5_search']:
-                ctx = [file for file in File.query.all() if file.md5.startswith(request.form['md5'][:32])]
+            ctx = [file for file in File.query.all() if file.md5.startswith(request.form['md5'][:32])]
 
-                return render_template(
-                    "md5/result.html",
-                    ctx=ctx
-                )
+            return render_template(
+                "md5/result.html",
+                ctx=ctx
+            )
         except KeyError:
             pass
 
@@ -50,25 +46,22 @@ def index():
 @bp.route("/report/<string:md5>", methods=['GET', 'POST'])
 def report(md5: str):
     if request.method == "GET":
-        session['csrf_md5_report'] = token_bytes(64).hex()
         return render_template(
-            "md5/report.html",
-            csrf=session['csrf_md5_report']
+            "md5/report.html"
         )
     if request.method == "POST":
         if request.referrer is None:
             abort(400)
         try:
-            if request.form['csrf_token'] == session['csrf_md5_report']:
-                ctx = Report(
-                    md5=md5,
-                    text=request.form['text']
-                )
-                db.session.add(ctx)
-                db.session.commit()
+            ctx = Report(
+                md5=md5,
+                text=request.form['text']
+            )
+            db.session.add(ctx)
+            db.session.commit()
 
-                webhook.send(f"{md5}에 대한 신고가 등록됨")
-                return redirect(url_for("index.index"))
+            webhook.send(f"{md5}에 대한 신고가 등록됨")
+            return redirect(url_for("index.index"))
         except KeyError:
             pass
         except IntegrityError:
