f5de80bea43dcbbf6bcfceabb5a1ac422a639ba4
chick_0 <chick_0@kakao.com>
2021-01-28 22:18:27 +0900

    웹훅 스크립트 변경

--- app/module/webhook.py
+++ app/module/webhook.py
@@ -2,9 +2,7 @@
 from json import dumps
 from urllib.request import Request, urlopen
 
-_id = "799874053916721162"
-_token = "UqBPPOnkL1_FiGOPDuRY8ui_h9s9WviMSsKmZhmHUKvBVVNS4U-FDA7C6I0ym314l-0R"
-url = f"https://discord.com/api/webhooks/{_id}/{_token}"
+from config import conf
 
 
 def send(message: str):
@@ -12,10 +10,11 @@ def send(message: str):
         "content": f"```\n{message}\n```"
     }
 
-    req = Request(url=url, method="POST",
-                  data=dumps(payload).encode("utf-8"))
+    req = Request(url=f"https://discord.com/api/webhooks/{conf['discord']['id']}/{conf['discord']['token']}",
+                  data=dumps(payload).encode("utf-8"),
+                  method="POST")
 
-    req.add_header("User-Agent", "curl")
+    req.add_header("User-Agent", "Python-urllib")
     req.add_header("Content-Type", "application/json")
 
     urlopen(req)
