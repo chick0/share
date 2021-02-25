16361507be7f2efac9e5a61cf1a5484274f90eca
chick_0 <chick_0@kakao.com>
2021-02-25 17:38:28 +0900

    시간 출력 추가

--- app/views/clean.py
+++ app/views/clean.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from hashlib import sha384
+from datetime import datetime
 
 from flask import Blueprint
 from flask import request
@@ -16,13 +17,15 @@ bp = Blueprint(
 
 @bp.route("")
 def clean():
+    time = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+
     with open(".SECRET_KEY", mode="rb") as fp:
         # 내부 인증 용도
         # - `SECRET_KEY` 가져오고 sha384로 해싱하기
         if request.headers.get("Secret-Key") != sha384(fp.read()).hexdigest():
-            return "Invalid Secret-Key", 403
+            return f"{time} - Invalid Secret-Key", 403
 
     db_remove()    # 보관 날짜가 지난 파일들 데이터베이스에서 삭제하기
     file_remove()  # 데이터베이스에 없는 파일들 업로드 풀더에서 삭제하기
 
-    return "OK"    # 아무 오류가 없었다면 `OK`
+    return f"{time} - OK"    # 아무 오류가 없었다면 `OK`
