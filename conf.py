58173ee1db38e8b6ccf13f64a4bf6b1160901fad
chick_0 <chick_0@kakao.com>
2021-02-25 17:51:34 +0900

    파일 수정

--- conf.py
+++ conf.py
@@ -2,11 +2,14 @@
 from os import path, listdir, mkdir
 from configparser import ConfigParser
 
-if not path.exists("conf"):
-    mkdir("conf")
+BASE_DIR = path.dirname(__file__)
+CONF_DIR = path.join(BASE_DIR, "conf")
+
+if not path.exists(CONF_DIR):
+    mkdir(CONF_DIR)
 
 conf = ConfigParser()
 conf.read(
-    filenames=[path.join("conf", f) for f in listdir(path.join("conf")) if f.endswith(".ini")],
+    filenames=[path.join(CONF_DIR, f) for f in listdir(path.join(CONF_DIR)) if f.endswith(".ini")],
     encoding="utf-8"
 )
